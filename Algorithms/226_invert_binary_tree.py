0f035de7bec752216c50a0b0e5656330328029bf
tactfulness <pvenkat.rv@gmial.com>
2018-01-26 15:22:48 +0530

    algorithms

---
+++ Algorithms/226_invert_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {TreeNode}
+    def invertTree(self, root):
+        if root == None: return root
+        
+        left = root.left
+        right = root.right
+        
+        root.left = self.invertTree(right)
+        root.right = self.invertTree(left)
+        
+        return root
